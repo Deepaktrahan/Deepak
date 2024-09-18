093a850421eb4863a24eeab483312499919c5ac9
Deepak <157271080+Deepaktrahan@users.noreply.github.com>
2024-09-18 13:48:05 +0530

    Add files via upload

---
+++ weather check website/index.js
@@ -0,0 +1,29 @@
+const apiKey = "23afaa2813f3b650c6989788020c6a6d";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+async function checkWeather(city = "bangalore") {
+    try {
+        const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+        if (!response.ok) {
+            throw new Error("City not found");
+        }
+        const data = await response.json();
+        console.log(data);
+
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value);
+});
+
+checkWeather();  // Initial call to check weather for the default city
